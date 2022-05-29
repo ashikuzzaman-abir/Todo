0d7a21c6851c83aa4f790526f93e6e654e5a08dc
Ashikuzzaman Abir <ashikuzzamanabir@outlook.com>
2022-05-29 23:48:02 +0600

    redux functionality added

--- src/state/reducers/todoList.reducer.js
+++ src/state/reducers/todoList.reducer.js
@@ -1,36 +1,54 @@
 import { createSlice } from "@reduxjs/toolkit/";
 
-const ISlists= {
-    mainList: [],
-    doneList:[],
-    searchOutput: [],
-}
-
+const ISlists = {
+	mainList: [],
+	doneList: [],
+	searchOutput: [],
+	currentSearch: "",
+};
 
 export const listSlice = createSlice({
-    name: "todoList",
-    initialState: ISlists,
-    reducers:{
-        addList: (state, action) => {
-            state.mainList.push(action.payload);
-        },
-        addDoneList: (state, action) => {
-            state.doneList.push(action.payload);
-        },
-        removeList: (state, action) => {
-            state.mainList= state.mainList.filter((item) => {
+	name: "todoList",
+	initialState: ISlists,
+	reducers: {
+		addList: (state, action) => {
+			state.mainList.push(action.payload);
+		},
+		addDoneList: (state, action) => {
+			state.doneList.push(action.payload);
+		},
+		removeList: (state, action) => {
+			state.mainList = state.mainList.filter((item) => {
 				return item !== action.payload;
 			});
-        },
-        removeDoneList: (state, action) => {
-            state.doneList= state.doneList.filter((item) => {
-                return item !== action.payload;
-            });
-        }
-    }
+		},
+		removeDoneList: (state, action) => {
+			state.doneList = state.doneList.filter((item) => {
+				return item !== action.payload;
+			});
+		},
+		setCurrentSearch: (state, action) => {
+			if (action.payload !== "") {
+				state.currentSearch = action.payload;
+			}
+		},
+		resetCurrentSearch: (state) => {
+			state.currentSearch = "";
+		},
+		setSearchOutput: (state, action) => {
+			state.searchOutput = action.payload;
+		},
+	},
+});
 
-    });
+export const {
+	addList,
+	addDoneList,
+	removeList,
+	removeDoneList,
+	setCurrentSearch,
+	resetCurrentSearch,
+	setSearchOutput,
+} = listSlice.actions;
 
-    export const { addList, addDoneList, removeList, removeDoneList } = listSlice.actions;
-
-    export default listSlice.reducer;
+export default listSlice.reducer;
